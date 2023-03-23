57e2e61746e7110e19b1707564be762824c8117f
Dev Pedro <pedrova69@gmail.com>
2023-03-23 20:19:40 -0300

    Agregar datos dummy

---
+++ backend/seeders/20230311180418-all.js
@@ -0,0 +1,30 @@
+'use strict';
+const users = require('./seeds/users.json');
+const items = require('./seeds/items.json');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('users', users);
+    await queryInterface.bulkInsert('items', items);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
